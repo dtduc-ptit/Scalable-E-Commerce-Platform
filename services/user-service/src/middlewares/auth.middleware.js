acc4a3476b4679287c4f3aa4ba34e91104d50a96
dtduc-ptit <dtduc.ptit@gmail.com>
2025-06-25 15:42:34 +0700

    feat: Thêm middleware check token

---
+++ services/user-service/src/middlewares/auth.middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const token = authHeader.split(' ')[1];
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: 'Forbidden' });
+    }
+};
+
+module.exports = auth;
