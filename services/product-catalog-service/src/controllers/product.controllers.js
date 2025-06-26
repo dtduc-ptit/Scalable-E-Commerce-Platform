e019b0576bce8383c3d27f190d7da0bcaf943040
dtduc-ptit <dtduc.ptit@gmail.com>
2025-06-26 10:42:31 +0700

    feat: Thêm api getbyid, update, delete product

--- services/product-catalog-service/src/controllers/product.controllers.js
+++ services/product-catalog-service/src/controllers/product.controllers.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product.models');
+const mongoose = require("mongoose");
 
 const createProduct = async (req, res) => {
     const { name, description, price, category, stock } = req.body
@@ -23,7 +24,10 @@ const createProduct = async (req, res) => {
                 message: 'Validation Error'
             });
         }
-        res.status(500).send("Internal server error")
+        res.status(500).send({
+          message: "Internal server error",
+          error: err.message
+        })
     }
 }
 
@@ -54,11 +58,95 @@ const getAllProduct = async (req, res) => {
     });
   } catch (err) {
     console.error('Error getting product list:', err);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).send({
+      message: "Internal server error",
+      error: err.message
+    })
   }
 };
 
+const getProductById = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({
+      message: `Invalid product ID format: ${req.params.id}`
+    });
+  }
+
+  try {
+    const product = await Product.findById(req.params.id);
+    if(!product) {
+      return res.status(404).json({ message: `Product with ID ${req.params.id} not found`});
+    }
+    res.json(product);
+  } catch(err) {
+      console.error('Error getting product:', err);
+      res.status(500).send({
+        message: "Internal server error",
+        error: err.message
+      })
+  }
+}
+
+const updateProduct = async (req, res) => {
+  const { name, description, price, category, stock } = req.body;
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      message: `Invalid product ID format: ${id}`
+    });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      { name, price, description, category, stock },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({
+        message: `Product with ID ${id} not found`
+      });
+    }
+
+    res.json({
+      message: `Updated product with ID ${id} successfully`,
+      data: updatedProduct
+    });
+  } catch (err) {
+    console.error("Error update product:", err);
+    res.status(500).json({
+      message: "Internal server error",
+      error: err.message
+    });
+  }
+};
+
+const removeProduct = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({
+      message: `Invalid product ID format: ${req.params.id}`
+    });
+  }
+  try {
+    const removeProduct = await Product.findByIdAndDelete(req.params.id);
+    if(!removeProduct) {
+      return res.status(404).json({ message: `Product with ID ${req.params.id} not found`});
+    }
+    res.json({message: `Product with ID ${req.params.id} deleted`})
+  } catch(err) {
+    console.error("Error update product:", err);
+    res.status(500).json({
+      message: "Internal server error",
+      error: err.message
+    });
+  }
+}
 module.exports = {
     getAllProduct,
-    createProduct
+    createProduct,
+    getProductById,
+    updateProduct,
+    removeProduct
 }
--- services/product-catalog-service/src/routes/product.routes.js
+++ services/product-catalog-service/src/routes/product.routes.js
@@ -5,5 +5,8 @@ const productController = require('../controllers/product.controllers');
 
 router.get('/', productController.getAllProduct);
 router.post('/', productController.createProduct);
+router.get('/:id', productController.getProductById);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.removeProduct);
 
 module.exports = router;
