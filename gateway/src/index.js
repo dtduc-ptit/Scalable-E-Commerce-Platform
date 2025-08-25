82bf555e98e98628dc51c74785353e3986ae4a28
dtduc-ptit <dtduc.ptit@gmail.com>
2025-08-25 09:13:53 +0700

    remove folder gateway

--- gateway/src/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`API Gateway running on port ${PORT}`);
-});
